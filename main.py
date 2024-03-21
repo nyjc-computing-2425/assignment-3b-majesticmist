a54c979f17d1d6cfaa70aa0954ec57a7ee845182
Steve Nguyen <154491875+majesticmist@users.noreply.github.com>
2024-03-21 12:02:40 +0000

    Final submission

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+x = stdform.find('x')
+mantissa = stdform[:x]
+exponent = stdform[x+4:]
+
+print(f'This number in E notation is {mantissa}E{exponent}.' )
